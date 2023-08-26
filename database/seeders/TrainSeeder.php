5cc838172f74ac718fba8446375668743320e5a0
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-08-26 18:32:10 +0200

    add function train

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -5,3 +5,14 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Train;
 use Faker\Generator as Faker;
+
+class TrainSeeder extends Seeder
+{
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $train = new Train();
+            $train->azienda = $faker->company;
+        }
+    }
+}
