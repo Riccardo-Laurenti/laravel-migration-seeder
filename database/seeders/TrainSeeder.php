edd024b5e07788a0c48812146abfac892c0d13a7
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-08-26 18:37:46 +0200

    finish function train

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -13,6 +13,15 @@ class TrainSeeder extends Seeder
         for ($i = 0; $i < 10; $i++) {
             $train = new Train();
             $train->azienda = $faker->company;
+            $train->stazione_partenza = $faker->city;
+            $train->stazione_arrivo = $faker->city;
+            $train->orario_partenza = $faker->time('H:i');
+            $train->orario_arrivo = $faker->time('H:i');
+            $train->codice_treno = $faker->unique()->randomNumber(5);
+            $train->numero_carrozze = $faker->numberBetween(1, 10);
+            $train->in_orario = $faker->boolean;
+            $train->cancellato = $faker->boolean;
+            $train->save();
         }
     }
 }
